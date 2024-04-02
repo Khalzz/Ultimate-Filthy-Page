c13256d3d88f87c357b322b8091129478aa546af
Rodrigo Seguel <63627567+Khalzz@users.noreply.github.com>
2024-04-02 20:52:04 -0300

    change

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -14,10 +14,10 @@ function CustomFooter() {
     <footer className={styles.footer}> 
         <p className={roboto.className + " " + styles.footer_text}>Made in Chile with love, passion and lots of caffeine.</p>
         <div className={styles.contact}>
-            <Link href="https://github.com/Khalzz" className={styles.contact_link}><Image className={styles.contact_img} src="Icons/Github.svg" width={500} height={500}/></Link>
-            <Link href="https://www.instagram.com/khalz_art/" className={styles.contact_link}><Image className={styles.contact_img} src="Icons/instagram.svg" width={500} height={500}/></Link>
-            <Link href="https://khalzdev.itch.io/arrowner" className={styles.contact_link}><Image className={styles.contact_img} src="Icons/itch.svg" width={500} height={500}/></Link>
-            <Link href="https://stackoverflow.com/users/15073892/khalz" className={styles.contact_link}><Image className={styles.contact_img} src="Icons/stackoverflow.svg" width={500} height={500}/></Link>
+            <Link href="https://github.com/Khalzz" className={styles.contact_link}><Image className={styles.contact_img} src="/Icons/Github.svg" width={500} height={500}/></Link>
+            <Link href="https://www.instagram.com/khalz_art/" className={styles.contact_link}><Image className={styles.contact_img} src="/Icons/instagram.svg" width={500} height={500}/></Link>
+            <Link href="https://khalzdev.itch.io/arrowner" className={styles.contact_link}><Image className={styles.contact_img} src="/Icons/itch.svg" width={500} height={500}/></Link>
+            <Link href="https://stackoverflow.com/users/15073892/khalz" className={styles.contact_link}><Image className={styles.contact_img} src="/Icons/stackoverflow.svg" width={500} height={500}/></Link>
           </div>
     </footer>
   );
